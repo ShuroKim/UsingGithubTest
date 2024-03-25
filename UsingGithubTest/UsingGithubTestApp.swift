a7d5a28bb12e84422e961a2e38f7dbc380a6ab4a
shuro <shuro0530@gmail.com>
2024-03-25 10:03:08 +0900

    Initial Commit

---
+++ UsingGithubTest/UsingGithubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingGithubTestApp.swift
+//  UsingGithubTest
+//
+//  Created by Shuraw on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct UsingGithubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
